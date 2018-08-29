40bccb2d8e2cb4ecb64401307965717e6f62a6b9
Justin Stewart <Justinstewart3313@gmail.com>
2018-08-29 15:12:28 -0600

    Finish suggest and populate dictionary

--- test/trie-test.js
+++ test/trie-test.js
@@ -1,10 +1,13 @@
 import { expect } from 'chai';
 import Trie from '../lib/trie';
-import Node from '../lib/node';
+
+import fs from 'fs';
 
 
 describe('TRIE', () => {
   let trie;
+  const text = "/usr/share/dict/words";
+  const dictionary = fs.readFileSync(text).toString().trim().split('\n');
 
   beforeEach ( () => {
     trie = new Trie();
@@ -27,13 +30,26 @@ describe('TRIE', () => {
     trie.insert('bigityBoop')
     expect(trie.count()).to.equal(1);
   })
- it ('should create branch for each unique first letter', () => {
+  it ('should create branch for each unique first letter', () => {
     trie.insert ('hello');
-    trie.insert ('cool')
-    trie.insert ('poop')
+
     // console.log(JSON.stringify(trie, null, 4))
-    expect(Object.keys(trie.root.children)).to.deep.eq([ 'h', 'c', 'p' ])
+    expect(trie.root).to.have.property( 'h')
   })
+  it('should suggest an array or words', () => {
+    trie.insert ('hello');
+    trie.insert ('help');
+    trie.insert ('poop');
+    let response = trie.suggest('he');
+    // console.log(JSON.stringify(trie, null, 4))
+    expect(response).to.deep.eq(['hello','help'])
+  })
+  it('should populate with words', () => {
+    trie.populate(dictionary);
+    expect(trie.totalWords).to.eq(235886)
+  })
+
+
 
 })
 
